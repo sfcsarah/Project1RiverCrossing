5ef16bab43dfd9984483ebbe467d240663f3297d
sfcsarah <sfc.sarah@gmail.com>
2016-01-30 15:46:22 -0800

    made containers and sprites

---
+++ js/script.js
@@ -0,0 +1,4 @@
+console.log("Javascript scripts loaded");
+console.log($('.container p'));
+//The above are tests to make sure that the local Jquery file and the
+// script file were loading correctly
