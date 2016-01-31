2009d36b90996eb8d172b28c578dd36377056a0a
sfcsarah <sfc.sarah@gmail.com>
2016-01-30 16:30:13 -0800

    testing toggling class onmouse click

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ console.log("Javascript scripts loaded");
 console.log($('.container p'));
 //The above are tests to make sure that the local Jquery file and the
 // script file were loading correctly
+var farmerLeft = document.getElementById('farmerLeft');
+farmerLeft.onclick=clickFarmer;
+function clickFarmer() {
+  console.log("farmer on left bank was clicked")
+  farmerLeft.classList.toggle("hidden");
+  farmerBoat.classList.toggle("hidden");
+}
