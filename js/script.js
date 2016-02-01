f88df8b66693a11645e30fd347525c75771d4848
sfcsarah <sfc.sarah@gmail.com>
2016-02-01 10:08:33 -0800

    appended farmer to boat

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-console.log("Javascript scripts loaded");
-console.log($('.container p'));
-//The above are tests to make sure that the local Jquery file and the
+var $paraJ = $('.container p')
+console.log('Javascript scripts loaded')
+console.log($paraJ)
+
+// The above are tests to make sure that the local Jquery file and the
 // script file were loading correctly
 var farmerLeft = document.getElementById('farmerLeft');
-farmerLeft.onclick=clickFarmer;
+var farmer = $('#farmerLeft');
+var boat = $('#boat');
+/*
+farmerLeft.onclick = clickFarmer;
 function clickFarmer() {
-  console.log("farmer on left bank was clicked")
-  farmerLeft.classList.toggle("hidden");
-  farmerBoat.classList.toggle("hidden");
+  console.log('farmer on left bank was clicked');
+  farmerLeft.classList.toggle('hidden');
+  farmerBoat.classList.toggle('hidden');
 }
+*/
+function moveRight(item) {
+  item.click(function() {
+    console.log(item+ "clicked");
+    boat.append(item);
+  })
+}
+moveRight(farmer);
+// the above will append the farmer to the boat
