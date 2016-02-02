53831c16aeb962f79fd5565ab3d2eb2b89490cb8
sfcsarah <sfc.sarah@gmail.com>
2016-02-01 16:53:33 -0800

    sameSide function, logic tests

--- js/script.js
+++ js/script.js
@@ -4,42 +4,71 @@ console.log($paraJ)
 
 // The above are tests to make sure that the local Jquery file and the
 // script file were loading correctly
-var farmerLeft = document.getElementById('farmerLeft');
-var farmer = $('#farmerLeft');
+var farmer = $('#farmer');
+var fox = $('#fox');
+var goose = $('#goose');
+var cabbage = $('#cabbage');
+
+var leftBankLoc = $('#leftBank');
 var boat = $('#boat');
-var leftBank = $('#leftBank');
-/*
-farmerLeft.onclick = clickFarmer;
-function clickFarmer() {
-  console.log('farmer on left bank was clicked');
-  farmerLeft.classList.toggle('hidden');
-  farmerBoat.classList.toggle('hidden');
-}
-*/
+var rightBankLoc = $('#rightBank');
+var buttonRiver = $('#crossRiver');
 
-/*
-function moveRight(item) {
-  item.click(function() {
-    console.log(item+ 'right clicked');
-    boat.append(item);
-  })
-}
-moveRight(farmer);
-*/
+buttonRiver.click(function() {
+  if (farmer.hasClass('boat')) {
+    crossTheRiver();
+    // function to check if valid move and add to turns
+  }
+  else {
+    console.log('the farmer needs to paddle the boat')
+  }
+})
 
-// the above will append the farmer to the boat
-function moveLeft(item) {
-  item.click(function() {
-    if (item.hasClass('leftBank') == true) {
-      console.log("hasClass is working")
+$('.sprite').click(function() {
+  console.log('sprite clicked');
+  console.log(this);
+  if ( $(this).hasClass('land') && boatHasSpace() && sameSide(this) ){
+    console.log("going from bank to boat");
+    boat.append($(this));
+    $(this).toggleClass('land');
+    $(this).toggleClass('boat');
+  }
+  else if ( $(this).hasClass('boat') && sameSide(this) ) {
+    console.log("going from boat to bank");
+    if ($(this).hasClass('left')) {
+      leftBankLoc.append($(this));
+      $(this).toggleClass('land');
+      $(this).toggleClass('boat');
     }
-    // the above tests the hasClass method in jQuery
-    console.log(item+ 'left clicked');
-    boat.append(item);
-    item.toggleClass('boat');
-    item.toggleClass('leftBank');
+  else if ($(this).hasClass('right')) {
+      rightBankLoc.append($(this));
+      $(this).toggleClass('land');
+      $(this).toggleClass('boat');
+    }
 
+  }
 
-  })
+})
+
+function boatHasSpace() {
+  if (boat.children().length < 2) {
+    return true;
+  }
+  return false;
 }
-moveLeft(farmer);
+
+function sameSide(item) {
+  if ( ($(item).hasClass('left') && boat.hasClass('left')) ||
+  ($(item).hasClass('right') && boat.hasClass('right')) ) {
+    return true;
+  }
+  console.log('boat is on the opposite side')
+  return false;
+}
+
+function crossTheRiver() {
+  boat.toggleClass('right');
+  boat.toggleClass('left');
+  boat.children().toggleClass('right');
+  boat.children().toggleClass('left');
+}
